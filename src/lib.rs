4915cdf5268545f5e0a9cc4060bb5b6d3edb2574
poopy <gonesbird@gmail.com>
2024-09-15 00:24:01 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -72,6 +72,7 @@ pub trait Action: Component {
     fn undo(&mut self, world: &mut World);
 }
 
+// TODO: use single `VecDeque` instead
 #[derive(Resource, Reflect, Default, Debug, Clone)]
 #[reflect(Resource, MapEntitiesResource, Default)]
 pub struct History {
